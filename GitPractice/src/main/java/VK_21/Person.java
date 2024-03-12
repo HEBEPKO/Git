0f2e6544dae4d983e29c8032e61cab4947e9f8ce
@aleksei_neverko <aaneverko@mail.ru>
2024-03-13 02:39:17 +0300

    Realese version 0.0.2

---
+++ GitPractice/src/main/java/VK_21/Person.java
@@ -0,0 +1,31 @@
+package VK_21;
+
+public class Person {
+    public String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    Person(String name,int age) {
+        this.name = name;
+        this.age = getAge();
+    }
+
+    public void printInfo() {
+        System.out.println("Меня завут - " + name + "\n Мой возраст " + getAge() + "лет");
+    }
+}
--- GitPractice/src/main/java/VK_21/Program.java
+++ GitPractice/src/main/java/VK_21/Program.java
@@ -1,43 +1,11 @@
 package VK_21;
 
-import java.util.Scanner;
-
 public class Program {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Создание фичи VK-21");
 
         Person person;
-        person = new Person("Olga",18);
+        person = new Person("Olga", 18);
         person.printInfo();
     }
-    class Person {
-        public String name;
-        private int age;
-
-        public String getName() {
-            return name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        Person(String name,int age) {
-            this.name = name;
-            this.age = getAge();
-        }
-
-        public void printInfo() {
-            System.out.println("Меня завут - " + name + "\n Мой возраст " + getAge() + "лет");
-        }
-    }
-
 }
